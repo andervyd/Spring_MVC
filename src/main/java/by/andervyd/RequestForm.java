abbbd95d7d3b17a739ef97dfa5ee219371a4c939
andervyd <andervyd@gmail.com>
2020-12-27 20:17:40 +0200

    added @RequestMapping for class

--- src/main/java/by/andervyd/RequestForm.java
+++ src/main/java/by/andervyd/RequestForm.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/request")
 public class RequestForm {
 
     @RequestMapping("/ask-details")
